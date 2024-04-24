1704246203e6b3f1dd963c99b67eb35e94f59ee6
realtherealer <realtherealer@gmail.com>
2024-04-24 21:44:46 +0100

    add sm64intp

--- dist/romlist.js
+++ dist/romlist.js
@@ -1,6 +1,7 @@
 var ROMLIST = [
     {url:"roms/Super Mario 64 (USA).z64",title:"Select a game"},
     {url:"roms/Super Mario 64 (USA).z64",title:"Super Mario 64"},
+    {url:"roms/SM64IP.z64",title:"Super Mario 64: Internal Plexus"},
     {url:"roms/Mario Kart 64 (USA).z64",title:"Mario Kart 64"},
     {url:"roms/Super Smash Bros. (USA).z64",title:"Super Smash Bros. N64"},
     {url:"roms/Paper Mario.z64",title:"Paper Mario N64"},
