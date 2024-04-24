5637bfeff30b0a0a4b8a94e02fa6b775b90ac213
realtherealer <realtherealer@gmail.com>
2024-04-24 22:02:07 +0100

    sm romhacks

--- dist/romlist.js
+++ dist/romlist.js
@@ -1,7 +1,8 @@
 var ROMLIST = [
     {url:"roms/Super Mario 64 (USA).z64",title:"Select a game"},
     {url:"roms/Super Mario 64 (USA).z64",title:"Super Mario 64"},
-    {url:"roms/SM64IP.z64",title:"Super Mario 64: Internal Plexus"},
+    {url:"roms/Super Mario 64 Star Road.z64",title:"Super Mario 64: Star Road"},
+    {url:"roms/TPP.z64",title:"Super Mario 64: TPP"},
     {url:"roms/Mario Kart 64 (USA).z64",title:"Mario Kart 64"},
     {url:"roms/Super Smash Bros. (USA).z64",title:"Super Smash Bros. N64"},
     {url:"roms/Paper Mario.z64",title:"Paper Mario N64"},
