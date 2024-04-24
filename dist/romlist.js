dcc52949d35c65243a6d4b4f180f13782c4da823
realtherealer <147657739+realtherealer@users.noreply.github.com>
2024-04-24 19:07:09 +0100

    Update romlist.js

--- dist/romlist.js
+++ dist/romlist.js
@@ -1,7 +1,7 @@
 var ROMLIST = [
-    /*
-    {url:"roms/rom1.z64",title:"Game 1"},
-    {url:"roms/rom2.v64",title:"Game 2"},
-    {url:"roms/rom3.v64",title:"Game 3"},
-    */
+    
+    {url:"roms/Super Mario 64 (USA).z64",title:"Super Mario 64"},
+    {url:"roms/Mario Kart 64 (USA).z64",title:"Mario Kart 64"},
+    {url:"roms/Super Smash Bros. (USA).z64",title:"Super Smash Bros. N64"},
+    
 ];
